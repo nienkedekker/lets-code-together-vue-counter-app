e940bbcc015aee8656b808b2687695b76dc0708e
Nienke Dekker <nienke.dekker@nos.nl>
2020-01-27 20:18:11 +0100

    initial setup

---
+++ README.md
@@ -0,0 +1,55 @@
+# About
+
+This app was created for the React/Vue workshop held during the 8 February 2020 Let's Meet Together meet-up.
+
+This is the *Vue* version for *beginners*: [find its React counterpart here](https://github.com/Marleintje/counter-app-react-vue-workshop-beginners). Looking for the advanced version? [Go here](https://todo).
+
+- Meet-up organizer: Vanessa Ho
+- Workshop organizers: Nienke Dekker & Marlein Rusch
+
+## Setup
+
+TODO
+
+## Used
+- git init
+- npm init
+- npm install -g @vue/cli
+- vue create, default settings
+
+## Workshop goals
+
+Participants should be able to answer the following questions:
+
+- Why use a front-end framework like React/Vue?
+- What are the most important differences between React and Vue?
+- What are React/Vue components?
+- What is state management?
+
+Participants can:
+
+- Set up a React or Vue app
+- Integrate CSS into the project
+- Make and apply a reusable component
+- Manage state (on component level)
+
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
+
+### Lints and fixes files
+```
+yarn run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+			<div class="container">
+				<h2 class="counter-text">{{ count }}</h2>
+				<!--If we want to directly listen to events emitted from Vue components, we have to use the "native" modifier. -->
+				<counter-button @click.native="incrementCount" title="Increment count"/>
+				<counter-button @click.native="decrementCount" title="Decrement count"/>
+				<counter-button @click.native="resetCount" title="Reset count"/>
+			</div>
+  </div>
+</template>
+
+<script>
+import CounterButton from './components/CounterButton.vue'
+
+export default {
+  name: 'app',
+  components: {
+		CounterButton
+	},
+	data() {
+		return {
+			count: 0
+		}
+	},
+	mounted() {
+  		console.log(this.count);
+	},
+	methods: {
+		incrementCount() {
+			this.count = this.count + 1;
+		},
+		decrementCount() {
+				this.count = this.count - 1;
+		},
+		resetCount() {
+				this.count = 0;
+		},
+	}
+}
+</script>
+
+<style>
+#app {
+	text-align: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+}
+
+.counter-text {
+	font-weight: normal;
+	font-size: 3rem;
+	margin-top: 0;
+	margin-bottom: 1.5rem;
+}
+
+.container {
+	width: 100%;
+	max-width: 800px;
+	padding: 1rem 15px;
+	margin: 0 auto;
+	background-color: #f8f9fa;
+	border: 1px solid rgba(184, 141, 152, 0.38);
+	border-radius: 0.25rem;
+}
+</style>
